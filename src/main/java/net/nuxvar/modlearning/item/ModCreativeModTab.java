91e8c3cbbd63c5ee3a8e3ad7abe081cbc605742b
Nuxvar <rafadela69@gmail.com>
2023-08-01 00:55:19 +0200

    Learning How to make Items

---
+++ src/main/java/net/nuxvar/modlearning/item/ModCreativeModTab.java
@@ -0,0 +1,14 @@
+package net.nuxvar.modlearning.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class ModCreativeModTab {
+    public static final CreativeModeTab LEARNING_TAB = new CreativeModeTab("learningtab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.PEACH.get());
+        }
+    };
+}
---
+++ src/main/java/net/nuxvar/modlearning/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.nuxvar.modlearning.item;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.nuxvar.modlearning.ModLearning;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ModLearning.MOD_ID);
+
+    public static final RegistryObject<Item> PEACH = ITEMS.register("peach",
+            () -> new Item(new Item.Properties().tab(ModCreativeModTab.LEARNING_TAB)));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
